76f5859fff263756c53e190169a7e02adb918f33
lucas <kjk727@me.com>
2020-01-15 17:32:56 +0900

    Protein translation in progress

--- protein-translation/protein-translation.js
+++ protein-translation/protein-translation.js
@@ -1,3 +1,18 @@
-export const translate = () => {
-    throw new Error('Remove this statement and implement this function')
+export const translate = (codon, index = 0, protein = []) => {
+    if (index < 3) {
+        return protein
+    } else {
+        translateCodon(codon, index)
+        translate(codon, index + 3, protein)
+    }
+}
+
+const translateCodon = (codon, index) => {
+    switch (codon.slice(index, index + 2)) {
+        case 'AUG':
+            break
+
+        default:
+            break
+    }
 }
