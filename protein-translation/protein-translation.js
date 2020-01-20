2e4648cb6964a34573b5ed38d26a2b8a802e67ce
Lucas <kjk727@me.com>
2020-01-20 22:53:45 +0900

    Protein first time solution

--- protein-translation/protein-translation.js
+++ protein-translation/protein-translation.js
@@ -1,18 +1,45 @@
-export const translate = (codon, index = 0, protein = []) => {
-    if (index < 3) {
-        return protein
-    } else {
-        translateCodon(codon, index)
-        translate(codon, index + 3, protein)
-    }
+export const translate = (codon = '') => {
+    let indexString = ''
+
+    if (codon.length <= 0) return []
+
+    if (codon.length % 3 !== 0) throw new Error('Invalid codon')
+
+    return [...codon].reduce((acc, cur, i, arr) => {
+        indexString += cur
+        if (indexString.length === 3) {
+            const rna = TRANSLATE_CODON[indexString]
+
+            if (!rna) {
+                throw new Error('Invalid codon')
+            } else if (rna === 'STOP') {
+                arr.splice(1)
+                return acc
+            }
+
+            acc.push(rna)
+            indexString = ''
+        }
+        return acc
+    }, [])
 }
 
-const translateCodon = (codon, index) => {
-    switch (codon.slice(index, index + 2)) {
-        case 'AUG':
-            break
-
-        default:
-            break
-    }
+const TRANSLATE_CODON = {
+    AUG: 'Methionine',
+    UUU: 'Phenylalanine',
+    UUC: 'Phenylalanine',
+    UUA: 'Leucine',
+    UUG: 'Leucine',
+    UCU: 'Serine',
+    UCC: 'Serine',
+    UCA: 'Serine',
+    UCG: 'Serine',
+    UAU: 'Tyrosine',
+    UAC: 'Tyrosine',
+    UGU: 'Cysteine',
+    UGC: 'Cysteine',
+    UGG: 'Tryptophan',
+    UAA: 'STOP',
+    UAG: 'STOP',
+    UGA: 'STOP'
 }
